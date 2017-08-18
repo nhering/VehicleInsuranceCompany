b222ca16e011f092d30094f329c913ad46912d98
nhering <nhering1@gmail.com>
2017-08-17 19:01:36 -0700

    Data annotations

--- InsuranceQuote/Models/DriverModel.cs
+++ InsuranceQuote/Models/DriverModel.cs
@@ -10,12 +10,19 @@ namespace InsuranceQuote.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
         [Display(Name ="First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+
+        [Required]
         public int ZipCode { get; set; }
 
         public ICollection<VehicleModel> Vehicles { get; set; }
